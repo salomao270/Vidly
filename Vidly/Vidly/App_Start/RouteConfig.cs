55f8b0d03de428fdbb486f694eca2f690d68a3a2
salomao <salomao270@hotmail.com>
2018-08-16 12:44:57 -0300

    Added some Actions to MoviesController and created a Custom Route on RouteConfig

--- Vidly/Vidly/App_Start/RouteConfig.cs
+++ Vidly/Vidly/App_Start/RouteConfig.cs
@@ -14,6 +14,14 @@ namespace Vidly
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                "MoviesByReleaseDate",
+                "movies/released/{year}/{month}",
+                new { controller = "Movies", action = "ByReleaseDate"},
+                // new { year = "\\d{4}", month = "\\d{2}" });      // without @"" must insert one more \ just like: \\d instead of \d
+                // new { year = @"\d{4}", month = @"\d{2}" });      // Regular Expression, \d represents digit number
+                new { year = @"2015|2016", month = @"\d{2}" });         // limiting year parameter from 2015 to 2016
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
--- Vidly/Vidly/Controllers/MoviesController.cs
+++ Vidly/Vidly/Controllers/MoviesController.cs
@@ -13,8 +13,36 @@ namespace Vidly.Controllers
         public ActionResult Random()
         {
             var movie = new Movie() { Name = "Shrek!" };
-
+            // return Content("Bonjour");
+            // return HttpNotFound();
+            // return new EmptyResult();
+            // return RedirectToAction("Index", "Home");   // (nameOfTheAction, nameOfTheController);
+            // return RedirectToAction("Index", "Home", new { page = 1, sortBy = "name" } );
             return View(movie);
+        }
+
+        public ActionResult ByReleaseDate(int year, int month)
+        {
+            return Content(year + "/" + month);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            return Content("id: " + id);
+        }
+
+        // Movies/Index
+        public ActionResult Index(int? pageIndex, string sortBy)
+        {
+            if (!pageIndex.HasValue)
+            {
+                pageIndex = 1;
+            }
+            if (String.IsNullOrWhiteSpace(sortBy))
+            {
+                sortBy = "Name";
+            }
+            return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
         }
     }
 }
