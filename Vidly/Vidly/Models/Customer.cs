f14c49dcd2feefa73593537ce842711d0b156464
salomao <salomao270@hotmail.com>
2018-08-25 18:26:52 -0300

    Created database using Code First concept.

--- Vidly/Vidly/Models/Customer.cs
+++ Vidly/Vidly/Models/Customer.cs
@@ -9,5 +9,8 @@ namespace Vidly.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public bool IsSubscribedToNewsletter { get; set; }
+        public MembershipType MembershipType { get; set; }      // its a navigation type, to navigate from Customer to its MembershipType
+        public byte MembershipTypeId { get; set; }              // Entity Framework recognizes this attribute as FK and apply it in database as FK.
     }
 }
