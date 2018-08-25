a5d2fff6109c6078b9ccb666729b71553165952b
salomao <salomao270@hotmail.com>
2018-08-25 19:19:30 -0300

    Replace hard code data (that was used for example) to new Customer table (Entity Framework database)

--- Vidly/Vidly/Controllers/CustomersController.cs
+++ Vidly/Vidly/Controllers/CustomersController.cs
@@ -10,12 +10,29 @@ namespace Vidly.Controllers
 {
     public class CustomersController : Controller
     {
+        private ApplicationDbContext _context;
+
+        public CustomersController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
         // GET: Customers
         // [Route("Customers")]     // it doesnt necessary marke this here, because in RourConfig.cs has already a default 
         // route with this argument: url: "{controller}/{action}/{id}"
         public ActionResult Index()
         {
-            var customers = GetCustomers();
+            // var customers = GetCustomers();
+
+            // this customer's property is a DB set defined in our DB context
+            // it really get customers from database when Entity Framework iterates over this object (its called deferred execution, for that reason we insert Tolist() method to gets at this time.
+            var customers = _context.Customers.ToList();
+
 
             return View(customers);
             // it was an ViewModel example
@@ -28,7 +45,8 @@ namespace Vidly.Controllers
         // [Route("Customers/{id}")]
         public ActionResult Details(int id)
         {
-            var customer = GetCustomers().SingleOrDefault(c => c.Id == id);
+            // var customer = GetCustomers().SingleOrDefault(c => c.Id == id);
+            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
 
             if (customer == null)
             {
@@ -36,16 +54,5 @@ namespace Vidly.Controllers
             }
             return View(customer);
         }
-
-
-        private IEnumerable<Customer> GetCustomers()
-        {
-            return new List<Customer>
-            {
-                new Customer { Id = 1, Name = "John Smith" },
-                new Customer { Id = 2, Name = "Mary Williams" }
-            };
-        }
-
     }
 }
