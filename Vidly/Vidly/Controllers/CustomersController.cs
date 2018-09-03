fb113a5266422cf9141202eaa3e7ccf3eb27a61b
salomao <salomao270@hotmail.com>
2018-09-03 20:19:37 -0300

    Add a form for adding and editing/updating Customers

--- Vidly/Vidly/Controllers/CustomersController.cs
+++ Vidly/Vidly/Controllers/CustomersController.cs
@@ -27,12 +27,52 @@ namespace Vidly.Controllers
         {
             var memberhsipTypes = _context.MembershipTypes.ToList();
 
-            var viewModel = new NewCustomerViewModel()
+            var viewModel = new CustomerFormViewModel()
             {
                 MembershipTypes = memberhsipTypes
             };
 
-            return View(viewModel);
+            return View("CustomerForm", viewModel);
+        }
+
+        // When New.cshtml form send their request data to this Action, ASP.NET MVC will bind that form request data to this viewModel object with all that values encapsulated in their object properties.
+        [HttpPost]
+        // public ActionResult Create(NewCustomerViewModel viewModel)
+        public ActionResult Save(Customer customer)       // because our form request data in CustomerForm.cshtml key values are Customer type, MVC could binds it as Customer object as well
+        {
+            // it is a new customer, so it is add process
+            if (customer.Id == 0)
+                _context.Customers.Add(customer);
+
+            // it is an existing customer, so it is update process based on its corresponding id
+            else
+            {
+                var customerInDb = _context.Customers.Single(c => c.Id == customer.Id);                
+                customerInDb.Name = customer.Name;
+                customerInDb.Birthdate = customer.Birthdate;
+                customerInDb.MembershipTypeId = customer.MembershipTypeId;
+                customerInDb.IsSubscribedToNewsletter = customer.IsSubscribedToNewsletter;
+            }
+            _context.SaveChanges();
+
+            return RedirectToAction("Index", "Customers");
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
+
+            if (customer == null)
+                return HttpNotFound();
+
+            var viewModel = new CustomerFormViewModel
+            {
+                Customer = customer,
+                MembershipTypes = _context.MembershipTypes.ToList()
+            };
+
+            // We can override the MVC convention, for convention the MVC search for the View with the same same of the Action name, but we can change it for another View result name as here)
+            return View("CustomerForm", viewModel);
         }
 
         // GET: Customers
