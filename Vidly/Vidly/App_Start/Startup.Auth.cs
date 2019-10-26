57c7eca1c77659599dc119a1385b550d3cf106b6
salomao <salomao270@hotmail.com>
2019-10-26 12:51:00 -0300

    Add publish profiles, move appSettings from webconfig to a fileConfig

--- Vidly/Vidly/App_Start/Startup.Auth.cs
+++ Vidly/Vidly/App_Start/Startup.Auth.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Configuration;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin;
@@ -55,8 +56,8 @@ namespace Vidly
             //   consumerSecret: "");
 
             app.UseFacebookAuthentication(
-               appId: "2670406993011796",
-               appSecret: "6bb9b88d40ccdd717e85833df8f4f0b2");
+               appId: ConfigurationManager.AppSettings["FacebookAppId"],
+               appSecret: ConfigurationManager.AppSettings["FacebookAppSecret"]);
 
             //app.UseGoogleAuthentication(new GoogleOAuth2AuthenticationOptions()
             //{
