84a3d1cfce3969558ee6dcac202a13686d86b509
salomao <salomao270@hotmail.com>
2019-10-26 16:49:38 -0300

    Apply final touch on CSS properties file and an Introduction web page.

--- Vidly/Vidly/App_Start/BundleConfig.cs
+++ Vidly/Vidly/App_Start/BundleConfig.cs
@@ -29,6 +29,7 @@ namespace Vidly
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap-lumen.css",
+                      "~/Content/bootstrap-theme-custom.css",
                       "~/Content/datatables/css/datatables.bootstrap.css",
                       "~/Content/typeahead.css",
                       "~/content/toastr.css",
